de73b6bba44c92ee701156cf2ca12c495eebe610
mitchellm <developer@mitchellmudd.dev>
2023-05-28 22:25:53 -0400

    Learned some stuff about generics :) !

--- src/chapter5-generics.ts
+++ src/chapter5-generics.ts
@@ -1,10 +1,103 @@
 // ! LESSON 29: I don't know what I want, but I know how to get it
 
+// ! The premise for this chapter is that we are building a video player portal that features video streams of differing quality, subtitles in different languages, and user centric features.
+
+type videoFormatUrls = {
+  format360p: URL;
+  format480p: URL;
+  format720p: URL;
+  format1080p: URL;
+};
+
+// We want to provide a function that can format URLs in different ways
+
+declare const videos: videoFormatUrls;
+declare function loadFormat(format: string): void;
+
+// to ensure that the format is one of the keys of the videoFormatUrls type, we can use a type predicate
+// This looks more complicated than a union type, but it is more flexible. If you added 4K video to the videoFormatUrls type, you would not need to update the isFormatAvailable function.
+function isFormatAvailable(
+  obj: videoFormatUrls,
+  key: string
+): key is keyof videoFormatUrls {
+  return key in obj;
+}
+
+// Now we have a similar situation, but for subtitles
+type subtitles = {
+  english: URL;
+  german: URL;
+  dutch: URL;
+  french: URL;
+};
+
+function subtitlesAvailable(
+  obj: subtitles,
+  key: string
+): key is keyof subtitles {
+  return key in obj;
+}
+
+// The above is repetitive, so we can use a generic type to make it more flexible
+// First let's look at how we would solve the above using just javascript
+// You would just pass in the object and the key you want to check and return key in obj
+// In typescript we can use a generic type to make this more flexible
+// Note that key could be a string, number, or symbol, so we need to use a union type and that Formats extends object because we want to ensure that Formats is an object, otherwise we get an error.
+// ! The extends keyword is used to constrain the generic type to a specific type otherwise it would be too flexible
+function isAvailable<Formats extends object>(
+  obj: Formats,
+  key: string | number | symbol
+): key is keyof Formats {
+  return key in obj;
+}
+
+// Notice how we can now use the same function for both videos and subtitles
+// Also, the type of the object is inferred by the type of the first argument passed to the function
+if (isAvailable<videoFormatUrls>(videos, "format360p")) {
+  loadFormat("format360p");
+}
+
+// I've already seen generics before in other code that I've read
+
+// This returns a promise with a generic type of number
+async function randomNumber() {
+  return Math.random();
+}
+
+// You can also instantiate an array with a generic type
+let notGenericArray: number[] = [];
+
+let genericArray: Array<number>;
+
+// The above use case for generics may not look very useful, but when you have an array that utilizes multiple types, using a generic with a union type can be very useful
+
+let genericUnionArray: Array<number | string | boolean>;
+
 // !-----------------------------------!
 // ! LESSON 30: Generic Constraints
 
+// The below type specifies an object that contains
+// strings as keys that return
+// URLs as values
+type URLList = {
+  [k: string]: URL;
+};
+
+// * Notice how we extend URLList instead of object because we want to ensure that the generic type is an object that contains strings as keys and URLs as values. This a great example of how to use generic constraints.
+function loadFile<Formats extends URLList>(
+  fileFormats: Formats,
+  format: string
+) {
+  if (isAvailable(fileFormats, format)) {
+    loadFormat(format);
+  }
+}
+
 // !-----------------------------------!
 // ! LESSON 31: Working with keys
+
+// ? What's the downside of the above URLList type?
+// You can pass in a string that doesn't correlate to an actual format such as "format9999p"
 
 // !-----------------------------------!
 // ! LESSON 32: Generic Mapped Types
