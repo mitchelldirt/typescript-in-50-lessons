3d3198b5de504d8122394a4e01bbc5da301ad3db
mitchellm <developer@mitchellmudd.dev>
2023-06-26 15:11:05 -0400

    Completed another lesson 😎

--- src/chapter7-thinking-in-types.ts
+++ src/chapter7-thinking-in-types.ts
@@ -56,6 +56,82 @@ function promisify2<Fun extends FunctionWithCallback>(
 // !-----------------------------------!
 // ! LESSON 44: JSONify
 
+class Serializer<T> {
+  serialize(inp: T): string {
+    return JSON.stringify(inp);
+  }
+
+  deserialize(inp: string): T {
+    return JSON.parse(inp);
+  }
+}
+
+// JSON.parse and JSON.stringify are useful for serializing and deserializing JSON data
+// It's actually faster to parse JSON data than it is to parse an object
+
+type Widget = {
+  toJSON(): {
+    kind: "Widget";
+    date: Date;
+  };
+};
+
+type Item = {
+  text: string;
+  count: number;
+  choice: "yes" | "no" | null;
+
+  // Dropping the functions from the object
+  func: () => void;
+
+  // We need to parse nested elements
+  nested: {
+    isSaved: boolean;
+    data: [1, undefined, 2];
+  };
+
+  // A pointer to another type of widget
+  widget: Widget;
+
+  // The same item references again
+  children?: Item[];
+};
+
+// There is a difference between JSON and JS Objects. Below we'll be showing that off using JSONified
+
+type JSONified<T> = JSONifiedValue<T extends { toJSON(): infer U } ? U : T>;
+
+type JSONifiedValue<T> = T extends string | number | boolean | null
+  ? T
+  : T extends Function
+  ? never
+  : T extends object
+  ? JSONifiedObject<T>
+  : T extends Array<infer U>
+  ? JSONifiedArray<U>
+  : never;
+
+// This is a mapped type where we run through each property and apply the JSONified type to it. This is also an example of a recursive type. Typescript allows non circular recursive types.
+type JSONifiedObject<T> = {
+  [K in keyof T]: JSONified<T[K]>;
+};
+
+// We need to remap undefined types to null
+type UndefinedAsNull<T> = T extends undefined ? null : T;
+
+type JSONifiedArray<T> = Array<JSONified<UndefinedAsNull<T>>>;
+
+/*
+Here's what it will look like in action
+I don't fully understand how this is useful, but the more I look at weird typescript magic the more I understand it, bit by bit.
+
+const itemSerializer = new Serializer<Item>();
+
+const serialization = itemSerializer.serialize(anItem);
+
+const obj = itemSerializer.deserialize(serialization);
+*/
+
 // !-----------------------------------!
 // ! LESSON 45: Service Definitions
 
